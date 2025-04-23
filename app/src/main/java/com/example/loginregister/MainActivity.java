eaac52094a624a8629f893f3427a0e720bbdb5d2
Laura Manso <laura.manso@estudiantat.upc.edu>
2025-04-23 18:33:19 +0200

    cuando apreto iniciar sesion, me va a la tienda

--- app/src/main/java/com/example/loginregister/MainActivity.java
+++ app/src/main/java/com/example/loginregister/MainActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -30,14 +31,18 @@ public class MainActivity extends AppCompatActivity {
         TextView signUpText = findViewById(R.id.signUp);
 
         // Configurar el evento onClick para el TextView
-        signUpText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Crear un Intent para iniciar la actividad RegisterActivity
-                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
-                startActivity(intent);
-            }
+        signUpText.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        });
+
+        // Referencia al botón de iniciar sesión
+        Button loginButton = findViewById(R.id.loginButton);
+
+        // Configurar el evento onClick para el botón de iniciar sesión
+        loginButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ShopActivity.class);
+            startActivity(intent);
         });
     }
-
 }
--- app/src/main/java/com/example/loginregister/Swagger/Swagger.java
+++ app/src/main/java/com/example/loginregister/Swagger/Swagger.java
@@ -1,5 +1,5 @@
 package com.example.loginregister.Swagger;
 
 public interface Swagger {
-    
+
 }
