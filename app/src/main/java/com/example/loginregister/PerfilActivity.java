a671796cc44bb76b663a0cf66aea76779e5af082
Omar089 <omar.boury@estudiantat.upc.edu>
2025-05-08 14:27:42 +0200

    Update: Creando el perfil para el usuario

--- app/src/main/java/com/example/loginregister/PerfilActivity.java
+++ app/src/main/java/com/example/loginregister/PerfilActivity.java
@@ -1,6 +1,10 @@
 package com.example.loginregister;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,13 @@ public class PerfilActivity extends AppCompatActivity {
 
     public String user;
     public String correo;
+    EditText txtNewPassword;
+    Button enviarBtn;
+    Button mostrarInputsBtn;
+
+    TextView correoTxtView;
+
+    TextView userTxtView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +28,33 @@ public class PerfilActivity extends AppCompatActivity {
 
         this.user = getIntent().getStringExtra("user");
         this.correo = getIntent().getStringExtra("correo");
+
+        correoTxtView = findViewById(R.id.correoTxtView);
+        userTxtView = findViewById(R.id.userTxtView);
+
+        correoTxtView.setText(this.correo);
+        userTxtView.setText(this.user);
+
+        txtNewPassword = findViewById(R.id.txtNewPassword);
+        enviarBtn = findViewById(R.id.enviarBtn);
+
+        // Están ocultos por XML, pero puedes reforzarlo aquí
+        txtNewPassword.setVisibility(View.GONE);
+        enviarBtn.setVisibility(View.GONE);
+
+    }
+
+    public void changePassClick(View view){
+        txtNewPassword.setVisibility(View.VISIBLE);
+        enviarBtn.setVisibility(View.VISIBLE);
+
+    }
+
+    public void deleteAccountClick(View view){
+
+    }
+
+    public void enviarClick(View view){
 
     }
 
