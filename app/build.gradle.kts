1cf22d4c9f1b5c75025037ea6b423579cf5e6041
bictorp <victor.pintado.leiva@estudiantat.upc.edu>
2025-05-09 18:42:45 +0200

    Update: activity_shop.xml (not finished)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -43,9 +44,12 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
+    implementation(libs.github.glide)
+    implementation(libs.gson)
     implementation ("com.airbnb.android:lottie:3.4.0")
 }
