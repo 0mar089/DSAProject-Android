a0b7f8c0bf4cb95d154dbc537987cd8fd906cb58
Laura Manso <laura.manso@estudiantat.upc.edu>
2025-05-14 16:18:48 +0200

    Diseño activity_detail

--- app/src/main/java/com/example/loginregister/CartActivity.java
+++ app/src/main/java/com/example/loginregister/CartActivity.java
@@ -1,4 +1,4 @@
-package com.example.loginregister.Swagger;
+package com.example.loginregister;
 
 import android.os.Bundle;
 
@@ -7,8 +7,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
-import com.example.loginregister.R;
 
 public class CartActivity extends AppCompatActivity {
 
